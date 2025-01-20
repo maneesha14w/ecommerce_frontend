71e4ead97f2e8023804cdb6782c04ea3a8f3159b
Maneesha Walisundera <maneesha14w@gmail.com>
2025-01-20 15:31:49 +0530

    chore: model fix and build file added.

--- packages/fake_store_api/lib/src/models/product.dart
+++ packages/fake_store_api/lib/src/models/product.dart
@@ -1,3 +1,9 @@
+import 'package:fake_store_api/src/models/rating.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product.g.dart';
+
+@JsonSerializable()
 class Product {
   const Product({
     required this.id,
@@ -6,12 +12,17 @@ class Product {
     required this.category,
     required this.description,
     required this.image,
+    required this.rating,
   });
 
   final int id;
   final String title;
   final double price;
-  final double category;
-  final double description;
-  final double image;
+  final String category;
+  final String description;
+  final String image;
+  final Rating rating;
+
+  factory Product.fromJson(Map<String, dynamic> json) =>
+      _$ProductFromJson(json);
 }
