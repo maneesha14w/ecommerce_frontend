dc7b4254939f502025fa3c5d633ab91321fc0042
Maneesha Walisundera <maneesha14w@gmail.com>
2025-01-25 22:44:17 +0530

    feat: products state complete.

---
+++ lib/logic/product/cubit/product_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fake_store_repository/fake_store_repository.dart'
+    show ProductRepository;
+import 'package:equatable/equatable.dart';
+import '../models/product.dart';
+
+part 'product_state.dart';
+
+class ProductCubit extends Cubit<ProductState> {
+  ProductCubit(this._productRepository) : super(ProductState());
+
+  final ProductRepository _productRepository;
+
+  Future<void> fetchProducts() async {
+    emit(state.copyWith(status: ProductListStatus.loading));
+    try {
+      final products = Product.fromRepository(
+        await _productRepository.getAllProducts(),
+      );
+      emit(
+        state.copyWith(
+          status: ProductListStatus.success,
+          products: products,
+        ),
+      );
+    } catch (e) {
+      emit(state.copyWith(status: ProductListStatus.failure));
+    }
+  }
+}
